d11324b0faad8558acdf5075bba02fc08d5ac7e3
newbrokers <almesto@gmail.com>
2026-08-08 18:54:15 +0300

    Align desktop app icons with product branding

--- Build-Sdk.bat
+++ Build-Sdk.bat
@@ -14,4 +14,17 @@ if errorlevel 1 (
   exit /b 1
 )
 
+set CSC=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\csc.exe
+if not exist "%CSC%" set CSC=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\csc.exe
+if not exist "%CSC%" (
+  echo Could not find csc.exe. Install .NET Framework developer tools or the .NET SDK.
+  exit /b 1
+)
+
+"%CSC%" /nologo /target:exe /optimize+ /out:"%~dp0IconMaker.exe" /reference:System.dll /reference:System.Core.dll /reference:System.Drawing.dll "%~dp0IconMaker.cs"
+if errorlevel 1 exit /b 1
+
+"%~dp0IconMaker.exe" "%~dp0website\public\images\products\snipcopy.png" "%~dp0SnipCopy.ico"
+if errorlevel 1 exit /b 1
+
 "%DOTNET_EXE%" build "%~dp0SnipCopy.csproj" -c Release -p:Platform=x64
--- Build.bat
+++ Build.bat
@@ -7,6 +7,6 @@ if not exist "%CSC%" (
 )
 "%CSC%" /nologo /target:exe /optimize+ /out:"%~dp0IconMaker.exe" /reference:System.dll /reference:System.Core.dll /reference:System.Drawing.dll "%~dp0IconMaker.cs"
 if errorlevel 1 exit /b 1
-"%~dp0IconMaker.exe" "%~dp0SnipCopyIcon.png" "%~dp0SnipCopy.ico"
+"%~dp0IconMaker.exe" "%~dp0website\public\images\products\snipcopy.png" "%~dp0SnipCopy.ico"
 if errorlevel 1 exit /b 1
 "%CSC%" /nologo /target:winexe /optimize+ /win32icon:"%~dp0SnipCopy.ico" /out:"%~dp0SnipCopy.exe" /reference:System.dll /reference:System.Core.dll /reference:System.Drawing.dll /reference:System.Numerics.dll /reference:System.Security.dll /reference:System.Windows.Forms.dll "%~dp0SnipCopy.cs"
